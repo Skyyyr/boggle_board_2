9ec7160f5c6ba19a92ff55c8e133f3347e40af1e
Skyyyr <emuswgluvr@gmail.com>
2022-06-02 20:56:08 -0700

    Imported Boggle Board 1 as a base

--- boggle_board.py
+++ boggle_board.py
@@ -1,11 +1,50 @@
-# You should re-use and modify your old BoggleBoard class to support the new requirements
+import csv
+import string
+from random import randint
+
+GRID_WIDTH = 4
+GRID_HEIGHT = 4
+GRID_DEFAULT = "_"
+ALPHABET = string.ascii_uppercase
+DICE_FILE_PATH = "data/dice.csv"
+
 
 class BoggleBoard:
+
     def __init__(self):
         pass
 
+    def __str__(self):
+        print(ALPHABET)
+        grid = ""
+        for x in range(GRID_WIDTH):
+            for y in range(GRID_HEIGHT):
+                grid += GRID_DEFAULT
+            grid += "\n"
+        return grid
+
     def shake(self):
-        pass
-  
-    def include_word(self):
-        pass
+        with open(DICE_FILE_PATH, newline='') as file:
+            rows_in_file = csv.DictReader(file)
+            rolled_letters = ""
+            for row in rows_in_file:
+                # print(row['dice'][randint(0, 5)])
+                rolled_letters += row['dice'][randint(0, 5)]
+            print(rolled_letters)
+            grid = ""
+            letter_tracker = 0
+            for x in range(GRID_WIDTH):
+                for y in range(GRID_HEIGHT):
+                    letter = rolled_letters[letter_tracker]
+                    if letter == "Q":
+                        letter += "u"
+                    grid += letter + "\t"
+                    letter_tracker += 1
+                grid += "\n"
+            return grid
+
+
+b = BoggleBoard()
+print(b)
+print("SHAKE IT!\n")
+print(b.shake())
